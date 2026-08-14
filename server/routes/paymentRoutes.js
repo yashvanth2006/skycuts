3592d592866caf26b349f91d971c35088531d2d5
yashvanth2006 <yashvanth2006k@gmail.com>
2026-08-14 19:39:33 +0530

    Fix: correct auth middleware import path in paymentRoutes

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createOrder, verifyPayment, webhookHandler } from '../controllers/paymentController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import { protect } from '../middleware/auth.js';
 
 const router = express.Router();
 
